df2f2fad59529ec89da4bd1f7da2df0c9c6c0efe
SajjaDRaaD <s.ebrahimiraad@KRJ-EBRAHIMI>
2023-06-12 14:54:59 +0330

    add builder to ScaledUnitOfMeasurFactory

--- UOM.Domain.Test/ScaledUnitOfMeasureTests.cs
+++ UOM.Domain.Test/ScaledUnitOfMeasureTests.cs
@@ -27,7 +27,7 @@ namespace UOM.Domain.Test
         [InlineData(50,50000)]
         public void converts_value_to_base_dimension(decimal valueToConvert, decimal expected)
         {
-            var unitOfMeasure = new ScaledUnitOfMeasureBuilder().WithScaledToBase(1000).Build();
+            var unitOfMeasure = CreateScaledUnitOfMeasure(x => x.WithScaledToBase(1000));
             
             var result = unitOfMeasure.ConvertToBase(valueToConvert);
             result.Should().Be(expected);
@@ -39,14 +39,20 @@ namespace UOM.Domain.Test
         [InlineData(0,0)]
         public void converts_value_to_another_scaled_unit(decimal valueToConvert, decimal expected)
         {
-            var centimeter = new ScaledUnitOfMeasureBuilder().WithTitle("Centimeter").WithScaledToBase(0.01M).Build();
-            var kilometer = new ScaledUnitOfMeasureBuilder().WithTitle("Kilometer").WithScaledToBase(1000M).Build();
+            var centimeter = CreateScaledUnitOfMeasure(x=> x.WithTitle("Centimeter").WithScaledToBase(0.01M));
+            var kilometer = CreateScaledUnitOfMeasure(x=> x.WithTitle("Kilometer").WithScaledToBase(1000));
 
             var valueInCentimeter = kilometer.Convert(centimeter,valueToConvert);
 
             valueInCentimeter.Should().Be(expected);
         }
 
-        
+        private ScaledUnitOfMeasure CreateScaledUnitOfMeasure(Action<ScaledUnitOfMeasureBuilder> builderConfig)
+        {
+            var builder = new ScaledUnitOfMeasureBuilder();
+            builderConfig.Invoke(builder);
+
+            return builder.Build();
+        }
     }
 }
